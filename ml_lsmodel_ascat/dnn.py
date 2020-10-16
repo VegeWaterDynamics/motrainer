b4e0666c042ecb377c14613e6c861291e2588b0b
xushanthu-2014 <shanx14@tsinghua.org.cn>
2020-10-16 11:50:52 +0200

    some tiny changes in dnn.py

--- ml_lsmodel_ascat/dnn.py
+++ ml_lsmodel_ascat/dnn.py
@@ -1,6 +1,3 @@
-import sys
-#sys.path.insert(0,'../ml_lsmodel_ascat') 
-
 import shap
 import logging
 import keras
@@ -10,24 +7,23 @@ import numpy as np
 import pickle
 from pathlib import Path
 from skopt.space import Real, Categorical, Integer 
+from scipy.stats.stats import pearsonr, spearmanr
 from model import keras_dnn
 from keras.models import load_model
 
-
 logger = logging.getLogger(__name__)
-
 
 class DNNTrain(object):
     def __init__(self, train_input, train_output, 
-#                 test_input, test_output,
+                 test_input, test_output,
 #                 vali_input, vali_output, 
                  resultpath='.'):
         self.train_input = train_input
         self.train_output = train_output
         self.test_input = test_input
         self.test_output = test_output
-        self.vali_input = vali_input
-        self.vali_output = vali_output
+#        self.vali_input = vali_input
+#        self.vali_output = vali_output
         self.resultpath = Path(resultpath)
         self.resultpath.mkdir(parents=True, exist_ok=True)
         self.dimensions = {'learning_rate': Real(low=5e-4, high=1e-2, prior='log-uniform',name='learning_rate'), 
@@ -48,13 +44,25 @@ class DNNTrain(object):
                 self.dimensions[key] = Categorical(categories=value, name=key)
 
     def normalize(self):
-        scaler = sklearn.preprocessing.StandardScaler()
-        self.train_input = scaler.fit_transform(self.train_input)
-        self.train_output = scaler.fit_transform(self.train_output)
-        self.test_input = scaler.fit_transform(self.test_input)
-        self.test_output = scaler.fit_transform(self.test_output)
-        self.vali_input = scaler.fit_transform(self.vali_input)
-        self.vali_output = scaler.fit_transform(self.vali_output)
+        # prenormalization for output (or label)
+        self.scaler_test_output = sklearn.preprocessing.StandardScaler()
+        self.test_output = self.scaler_test_output.fit_transform(self.test_output)
+        
+        self.scaler_test_input = sklearn.preprocessing.StandardScaler()
+        self.test_input = self.scaler_test_input.fit_transform(self.test_input)
+        
+        self.scaler_train_output = sklearn.preprocessing.StandardScaler()
+        self.train_output = self.scaler_train_output.fit_transform(self.train_output)
+        
+        self.scaler_train_input = sklearn.preprocessing.StandardScaler()
+        self.train_input = self.scaler_train_input.fit_transform(self.train_input)
+#        scaler = sklearn.preprocessing.StandardScaler()
+#        self.train_input = scaler.fit_transform(self.train_input)
+#        self.train_output = scaler.fit_transform(self.train_output)
+#        self.test_input = scaler.fit_transform(self.test_input)
+#        self.test_output = scaler.fit_transform(self.test_output)
+#        self.vali_input = scaler.fit_transform(self.vali_input)
+#        self.vali_output = scaler.fit_transform(self.vali_output)
 
     def optimize(self, 
                  best_loss=1, 
@@ -80,13 +88,14 @@ class DNNTrain(object):
                                 batch_size=dimensions['batch_size'],
                                 callbacks=[earlystop],
                                 verbose=0,
-                                validation_data=validation_method
+                                validation_split=validation_method
                                 )
             # Get loss
-            loss = blackbox.history['loss'][-1]
+            loss = blackbox.history['val_loss'][-1]
             if loss < self.best_loss:
                 self.model = model
                 self.best_loss = loss
+                self.hehe = 1
             del model
             keras.backend.clear_session()
             return loss
@@ -98,12 +107,34 @@ class DNNTrain(object):
                                     n_jobs=n_jobs,
                                     kappa = kappa,
                                     x0=x0)
-    #=====================================================#
+    
     # test/predict using new data
-    def predict(self, model, input_data):
+    def predict(self, input_data):
 #        self.test_output = self.model.predict(self.test)
         output_data = self.model.predict(input_data)
         return output_data
+    
+    # performance calculation
+    def performance(self, scaler, method, predicted):
+#        scaler = self.scaler_test_output
+        re_predicted = scaler.inverse_transform(predicted,'f')
+        re_label     = scaler.inverse_transform(self.test_output,'f')
+        
+        difference = re_predicted - re_label
+        performance = np.zeros([predicted.shape[1],1])
+        if method == 'rmse':
+            for j in range(predicted.shape[1]):
+                performance[j,0] = np.round(np.sqrt(((difference[j]) ** 2).mean()),5)
+        elif method == 'mae':
+            for j in range(predicted.shape[1]):
+                performance[j,0] = np.round((difference[j].mean()),5)
+        elif method == 'pearson':
+            for j in range(predicted.shape[1]):
+                performance[j,0] = np.round(pearsonr(re_predicted[j], re_label[j]),5)[0]
+        elif method == 'spearman':
+            for j in range(predicted.shape[1]):
+                performance[j,0] = np.round(spearmanr(re_predicted[j], re_label[j]),5)[0]
+        return performance
     
     #=====================================================#
     def export_model(self, suffix_gpi='', suffix_year=''):
@@ -117,7 +148,7 @@ class DNNTrain(object):
         model = load_model('{}/{}/optimized_model_{}'.format(path_model.as_posix(), suffix_gpi, suffix_year))
         return model
     
-    #===============shap values calculation==================#
+    # shap values calculation
     def get_shap_values(self, model, input_whole):
         background = input_whole[np.random.choice(input_whole.shape[0], 1000, replace=False)]
         
@@ -133,3 +164,28 @@ class DNNTrain(object):
             pickle.dump([zip(self.shap_values[0][:,0], self.shap_values[0][:,1], self.shap_values[0][:,2], self.shap_values[0][:,3], 
                              self.shap_values[1][:,0],self.shap_values[1][:,1],self.shap_values[1][:,2], self.shap_values[1][:,3], 
                              self.shap_values[2][:,0],self.shap_values[2][:,1],self.shap_values[2][:,2], self.shap_values[2][:,3],)], f)
+
+#=============================================================================#
+# independent function
+def save_performance(result_path, performance, method,
+                         suffix_gpi):
+    path_model = Path(result_path)# / 'shap_values'
+    with open('{}/{}/{}'.format(path_model.as_posix(), 
+              suffix_gpi, method), 'wb') as f:
+        pickle.dump(performance, f)
+
+def get_shap_values(model, input_whole):
+    background = input_whole[np.random.choice(input_whole.shape[0], 1000, replace=False)]
+        
+    shap.initjs()
+    e = shap.DeepExplainer(model, background)
+    shap_values = e.shap_values(input_whole)
+        
+    return shap_values
+    
+def export_shap_values(result_path, shap_values, suffix_gpi,suffix_year):
+    path_model = Path(result_path)# / 'shap_values'
+    with open('{}/{}/Shap_values_{}'.format(path_model.as_posix(), suffix_gpi, suffix_year), 'wb') as f:
+        pickle.dump([zip(shap_values[0][:,0], shap_values[0][:,1], shap_values[0][:,2], shap_values[0][:,3], 
+                         shap_values[1][:,0], shap_values[1][:,1], shap_values[1][:,2], shap_values[1][:,3], 
+                         shap_values[2][:,0], shap_values[2][:,1], shap_values[2][:,2], shap_values[2][:,3],)], f)
