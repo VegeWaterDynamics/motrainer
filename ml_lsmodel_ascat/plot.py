c5d14951c4d17033d0c822d94723d4bc00cb4836
rogerkuou <rogerkuou@gmail.com>
2020-11-03 23:45:34 +0100

    add time series plot function

--- ml_lsmodel_ascat/plot.py
+++ ml_lsmodel_ascat/plot.py
@@ -7,6 +7,8 @@ from copy import copy
 
 def plot_gsdata(data,
                 nrowcol,
+                outpath,
+                outformat='jpeg',
                 titles=None,
                 rowlabels=None,
                 kw_padding=None,
@@ -94,7 +96,7 @@ def plot_gsdata(data,
                              subplot_kw={'projection': plot_crs})
     if kw_padding is not None:
         plt.tight_layout(**kw_padding)
-    feature_id = 0
+    subplotid = 0
     features = data.columns[2:]
     for ax in axes.flat:
         ax.set_extent(basemap_extent)
@@ -105,7 +107,7 @@ def plot_gsdata(data,
         sc = ax.scatter(data['lon'].values,
                         data['lat'].values,
                         marker='o',
-                        c=data[features[feature_id]],
+                        c=data[features[subplotid]],
                         transform=data_crs,
                         cmap=colormap,
                         s=5)
@@ -120,7 +122,7 @@ def plot_gsdata(data,
         ax.gridlines(xlocs=xticks, ylocs=yticks)
         ax.xaxis.set_major_formatter(cartopy.mpl.gridliner.LONGITUDE_FORMATTER)
         ax.yaxis.set_major_formatter(cartopy.mpl.gridliner.LATITUDE_FORMATTER)
-        if feature_id % nrowcol[1] == 0:
+        if subplotid % nrowcol[1] == 0:
             _lambert_yticks(ax, yticks, 'left', fontsize)
         _lambert_xticks(ax, xticks, 'bottom', fontsize)
 
@@ -132,7 +134,7 @@ def plot_gsdata(data,
             cbar = fig.colorbar(sc, cax=cbar_ax)
             cbar.ax.tick_params(labelsize=fontsize / 2)
         elif cbar_mode == 'row':
-            if feature_id % nrowcol[1] == nrowcol[1] - 1:
+            if subplotid % nrowcol[1] == nrowcol[1] - 1:
                 axpos = ax.get_position()
                 cbar_ax = fig.add_axes(
                     [axpos.x1, axpos.y0, 0.01, axpos.height])  # l, b, w, h
@@ -140,23 +142,23 @@ def plot_gsdata(data,
                 cbar.ax.tick_params(labelsize=fontsize / 2)
 
         # Title
-        title_text = '(' + chr(97 + feature_id) + ') '  # Alphabet label
+        title_text = '(' + chr(97 + subplotid) + ') '  # Alphabet label
         if isinstance(titles, list):
-            title_text = title_text + titles[feature_id]
+            title_text = title_text + titles[subplotid]
         ax.set_title(label=title_text, fontsize=fontsize, loc='center')
 
         # Row label
         if rowlabels is not None:
             if cbar_mode == 'plot' or cbar_mode == 'row':
-                if feature_id % nrowcol[1] == nrowcol[1] - 1:
-                    cbar.set_label(rowlabels[int(feature_id / nrowcol[1])],
+                if subplotid % nrowcol[1] == nrowcol[1] - 1:
+                    cbar.set_label(rowlabels[int(subplotid / nrowcol[1])],
                                    fontsize=fontsize)
             else:
-                if feature_id % nrowcol[1] == 0:
-                    ax.set_ylabel(rowlabels[int(feature_id / nrowcol[1])],
+                if subplotid % nrowcol[1] == 0:
+                    ax.set_ylabel(rowlabels[int(subplotid / nrowcol[1])],
                                   fontsize=fontsize)
 
-        feature_id += 1
+        subplotid += 1
 
     # Colorbar for the entire figure
     if cbar_mode == 'fig':
@@ -166,7 +168,96 @@ def plot_gsdata(data,
         if cbar_label is not None:
             cax.set_xlabel(cbar_label, fontdict={'size': fontsize})
 
-    plt.savefig('testfig.jpg', bbox_inches='tight', format='jpeg')
+    plt.savefig(outpath, bbox_inches='tight', format=outformat)
+
+
+def plot_tsdata(data,
+                nrowcol,
+                outpath,
+                outformat='jpeg',
+                figsize=None,
+                linecolor=None,
+                titles=None,
+                rowlabels=None,
+                kw_padding=None,
+                fontsize=8):
+    """plot time-series data
+
+    :param data: list of DataFrames. Each DataFrames in the list will
+        be plotted into a subplot. The row index of DataFrame should be
+        DatetimeIndex. The columns will be ploted
+    :type data: list
+    :param nrowcol: number of (row, cloumn) of the subplots.
+    :type nrowcol: tuple
+    :param outpath: output path
+    :type outpath: str
+    :param outformat: outformat, defaults to 'jpeg'
+    :type outformat: str, optional
+    :param figsize: figure size in inches, defaults to None
+    :type figsize: tuple or list, optional
+    :param linecolor: line color per column in data, defaults to None
+    :type linecolor: dict, optional
+    :param titles: Titles of each plot, defaults to None
+    :type titles: list, optional
+    :param rowlabels: row labels, defaults to None
+    :type rowlabels: list, optional
+    :param kw_padding: Keyword arguement for pyplot.tight_layout().
+        Adjust subplot padding.
+        E.g. {'pad': 5, 'h_pad': 5, 'w_pad': 3}. defaults to None.
+    :type kw_padding: dict, optional
+    :param fontsize: fontsize of tick, title and labels,
+        defaults to 8.
+    :type fontsize: int, optional
+    """
+
+    fig, axes = plt.subplots(nrows=nrowcol[0], ncols=nrowcol[1])
+
+    if kw_padding is not None:
+        plt.tight_layout(**kw_padding)
+
+    if figsize is not None:
+        fig.set_size_inches(figsize[0], figsize[1])
+
+    subplotid = 0
+    linelist = dict()
+    for ax in axes.flat:
+        # Plot
+        for col in data[subplotid].columns:
+            if (linecolor is not None) and (col in linecolor.keys()):
+                lc = linecolor[col]
+            else:
+                lc = ['b', 'g', 'r', 'c', 'm', 'y',
+                      'k'][data[subplotid].columns.get_loc(col)]
+            line = ax.plot(data[subplotid][col], color=lc)
+            linelist[col] = line[0]
+            ax.tick_params(axis='both', labelsize=fontsize / 2)
+
+        # Title
+        title_text = '(' + chr(97 + subplotid) + ') '  # Alphabet label
+        if isinstance(titles, list):
+            title_text = title_text + titles[subplotid]
+        ax.set_title(label=title_text, fontsize=fontsize, loc='center')
+
+        # Row label
+        if rowlabels is not None:
+            if subplotid % nrowcol[1] == 0:
+                ax.set_ylabel(rowlabels[int(subplotid / nrowcol[1])],
+                              fontsize=fontsize)
+
+        subplotid += 1
+
+    # Legend
+    fig.legend(handles=linelist.values(),
+               labels=linelist.keys(),
+               fontsize=fontsize,
+               ncol=min([5, len(linelist)]),
+               loc="lower center")
+    fig.subplots_adjust(left=0.07,
+                        right=0.93,
+                        wspace=0.15,
+                        hspace=0.35,
+                        bottom=0.15)
+    plt.savefig(outpath, bbox_inches='tight', format=outformat)
 
 
 def _lambert_xticks(ax, ticks, tick_location, tickfontsize):
