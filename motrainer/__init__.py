47f352199678841f224bd2e183c3894467143c35
rogerkuou <o.ku@esciencecenter.nl>
2022-12-02 15:40:51 +0100

    simplify versioning

--- motrainer/__init__.py
+++ motrainer/__init__.py
@@ -2,9 +2,5 @@
 
 import logging
 
-from .__version__ import __version__
-
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__author__ = "Netherlands eScience Center, TUDelft"
-__email__ = 'o.ku@esciencecenter.nl'
