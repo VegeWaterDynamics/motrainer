7880fcd5ddde9f166b6e1609b8f0fe0b574c5f94
rogerkuou <o.ku@esciencecenter.nl>
2023-01-16 11:39:00 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'pytest-runner',
         # dependencies for `python setup.py build_sphinx`
         'sphinx',
-        'sphinx_rtd_theme',
         'recommonmark'
     ],
     tests_require=[
