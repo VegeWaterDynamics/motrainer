d31b698a23b076927b11dc3b48f5f64d947d4476
Ou Ku <o.ku@esciencecenter.nl>
2023-10-04 03:00:34 +0200

    initial implementation of model split

--- motrainer/motrainer.py
+++ motrainer/motrainer.py
@@ -1,8 +1,9 @@
 import warnings
 import xarray as xr
 import numpy as np
+import dask
 
-DIMS = ["space", "time"]  # Expected xr.Dataset dimensions
+MOT_DIMS = ["space", "time"]  # Expected xr.Dataset dimensions
 
 
 @xr.register_dataset_accessor("mot")
@@ -28,12 +29,12 @@ class MOTrainerDataset:
         # Dimension size should be 2
         if len(self._obj.dims) != 2:
             warnings.warn(
-                f'Dataset should have two dims: "space" and "time"', UserWarning
+                f'Dataset should have two dimensions: "space" and "time"', UserWarning
             )
             flag_valid = False
 
         # space and time dimensions should exist
-        for dim in DIMS:
+        for dim in MOT_DIMS:
             if not (dim in self._obj.dims):
                 warnings.warn(f"{dim} not found in the dimensions", UserWarning)
                 flag_valid = False
@@ -46,8 +47,38 @@ class MOTrainerDataset:
 
         return flag_valid
 
-    def model_split(self, identifier: dict = {}, chunks: tuple = ()):
-        # if not(identifier) and not(chunks):
-        #     raise ValueError('Need to specify "identifier" or chunks')
+    def model_split(self, identifier: dict | str):
+        
+        ds = self._obj
 
-        pass
+        list_id = []
+        for key in MOT_DIMS:
+            if key in identifier.keys():
+                key_id = key + "_id"
+                ds = ds.assign_coords({key_id: (key, identifier[key])})
+                list_id.append(key_id)
+
+        # Get the name of attributes to groupby
+        if len(list_id) > 1:
+            # Use space time cls coordinates as multi index
+            # Must stack on MOT_DIMS to reduce dims of data variable
+            multi_idx = ds.stack(samples=list_id).samples.values
+            ds = (
+                ds.reset_index(MOT_DIMS)
+                .stack(
+                    samples=MOT_DIMS, create_index=False
+                )  # No index creation since this will be added next.
+                .assign_coords(samples=multi_idx)
+                .set_xindex(list_id)
+            )
+            key_gb = "samples"
+        else:
+            key_gb = list_id[0]
+
+        # Groupby and separate to Dask Databags
+        list_db = []
+        for grp in list(ds.groupby(key_gb)):
+            list_db.append(grp[1])
+        bags = dask.bag.from_sequence(list_db)
+
+        return bags
